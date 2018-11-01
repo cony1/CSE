1fd867bacaeb365bd5fbb53ebcb7999b874b956e
cony1 <42385425+cony1@users.noreply.github.com>
2018-11-01 09:45:30 -0700

    11.1.18

---
+++ notes/Nico Martin - Guess Game.py
@@ -0,0 +1,1 @@
+print ("Guess a random number. You 5 guess's to find out")
--- notes/hello world.py
+++ notes/hello world.py
@@ -140,3 +140,13 @@ print(your_grade)
 # "Random" Notes
 import random # This should be on line 1
 print(random.randint(0,100))
+
+# Equality Statements
+print (5 > 3)
+print (5 >= 3)
+print (3 == 3)
+print (3 != 4)
+"""
+a = 3 # A is set to 3
+a == 3 #Is a equal to 3?
+"""
