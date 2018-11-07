a12e305da91683b2e3bf5ea584cbe6be5df4e974
cony1 <42385425+cony1@users.noreply.github.com>
2018-11-07 09:45:33 -0800

    11.7.18

--- notes/Nico Martin - Guess Game.py
+++ notes/Nico Martin - Guess Game.py
@@ -1,1 +1,10 @@
+import random
+r = random.randint(1,100)
 print ("Guess a random number. You 5 guess's to find out")
+
+if guess > random.randint(1,100):
+
+guesses_left = 5
+playing = True
+while guesses_left > 0 and playing:
+    guess = input
