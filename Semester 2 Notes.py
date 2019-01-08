b5b537fcdddb89b2c6b7ea2bd4ee5ac9d3d0278a
cony1 <42385425+cony1@users.noreply.github.com>
2019-01-08 09:44:52 -0800

    1.8.19

---
+++ Semester 2 Notes.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+# Hello World
+
+cars = 5
+driving = True
+
+print("I have %d cars" % cars)
+print("I have" + str(cars) + "cars")
+
+age = input("How old are you?")
+print(age + "??? You really shouldn't lie about your age.")
+
+colors = ["blue", "red", "white", "black"]
+colors.append("green")
+print(colors)
+colors.pop(0)
+print(colors)
+print(colors[2])
+
+print(len(colors))
