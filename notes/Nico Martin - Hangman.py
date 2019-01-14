456f8358895a4043d4178c01c98e5045a11c4516
cony1 <42385425+cony1@users.noreply.github.com>
2019-01-14 09:45:05 -0800

    1.14.19

--- notes/Nico Martin - Hangman.py
+++ notes/Nico Martin - Hangman.py
@@ -1,9 +1,13 @@
 import random
-
-word_list = ["Camera, Games, PS4, XBOX, iPhone, Android, Tablet, Car, Computer, Tv"]
-
-print(word_list)
 guesses = 8
 
-word = random.choice(word_list)
-print(word)
+word_list = ["Camera", "Games", "PS4", "XBox", "iPhone", "Android", "Tablet", "Car", "Computer", "Tv"]
+
+hangman_list = random.choice(word_list)
+
+print(hangman_list)
+
+print (len(hangman_list))
+
+if len == 5:
+    print("[],[],[],[],[],")
