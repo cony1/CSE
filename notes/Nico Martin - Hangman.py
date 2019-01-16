0efb2e45d74c87db0e1bbdb0a749454836054b5d
cony1 <42385425+cony1@users.noreply.github.com>
2019-01-16 09:44:38 -0800

    1.16.19

--- notes/Nico Martin - Hangman.py
+++ notes/Nico Martin - Hangman.py
@@ -1,16 +1,15 @@
 import random
+import string
 guesses = 8
-
 word_list = ["Camera", "Games", "PS4", "XBox", "iPhone", "Android", "Tablet", "Car", "Computer", "Tv"]
 
 hangman_list = random.choice(word_list)
 
 print(hangman_list)
 
-print (len(hangman_list))
-
-import string
+print(len(hangman_list))
 
 print(list(string.ascii_letters))
-print(string.digits)
 print(string.punctuation)
+
+for i in range(len(hangman_list)):
