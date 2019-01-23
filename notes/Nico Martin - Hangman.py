7e84152a923d2a13ffbc9518cf44a193877f18f8
cony1 <42385425+cony1@users.noreply.github.com>
2019-01-23 09:43:28 -0800

    1.23.19

--- notes/Nico Martin - Hangman.py
+++ notes/Nico Martin - Hangman.py
@@ -4,22 +4,13 @@ import string
 word_list = ["Camera", "Games", "PS4", "XBox", "iPhone", "Android", "Tablet", "Car", "Computer", "Tv"]
 
 guesses = 8
-LettersCorrect = []
-LettersIncorrect = []
+letters_guessed = []
 
+answer = random.choice(word_list)
+print(answer)
 
-hangman_list = random.choice(word_list)
+# print(list(string.ascii_letters))
+# print(string.punctuation)
 
-print(hangman_list)
+input(letters_guessed)
 
-print(len(hangman_list))
-
-print(list(string.ascii_letters))
-print(string.punctuation)
-
-for i in range(len(hangman_list)):
-    if hangman_list[i] == string.ascii_letters:
-        hangman_list.pop(i)
-        hangman_list.insert(i, "*")
-
-print(hangman_list)
