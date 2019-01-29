be392a2b165365384697167b11fa15d8a82ccdaf
cony1 <42385425+cony1@users.noreply.github.com>
2019-01-29 09:45:37 -0800

    1.29.19

---
+++ Dictionary.py
@@ -0,0 +1,57 @@
+states = {
+    "CA": "California",
+    "FL": "Florida",
+    "AK": "Alaska",
+    "GA": "Georgia"
+}
+
+print(states["CA"])
+print(states["AK"])
+
+complex_dictionary = {
+    "CA": {
+        "NAME": "California",
+        "POPULATION": 39500000,
+        "CITIES": [
+            "Fresno",
+            "San Francisco",
+            "Los Angees"
+        ]
+    },
+    "FL": {
+        "NAME": "Florida",
+        "POPULATION": 21300000,
+        "CITIES": [
+            "Miami",
+            "Orlando",
+            "Tampa",
+            "Jacksonville"
+        ]
+    },
+    "AK": {
+        "NAME": "Alaska",
+        "POPULATION": 737000,
+        "CITIES": [
+            "Anchorage",
+            "Fairbanks",
+            "Juneau"
+        ]
+    },
+    "GA": {
+        "NAME": "Georgia",
+        "POPULATION": 10500000,
+        "CITIES": [
+            "Atlanta",
+            "Savannah",
+            "Augusta"
+        ]
+    }
+}
+
+print(complex_dictionary["GA"]["POPULATION"])
+print(complex_dictionary["FL"]["NAME"])
+
+georgia = complex_dictionary["GA"]
+print(georgia)
+
+print(complex_dictionary["AK"]["CITIES"][0])
