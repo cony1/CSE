348c6b58445b8ddd8eacb4893092494629ab4990
cony1 <42385425+cony1@users.noreply.github.com>
2019-01-31 09:44:38 -0800

    1.31.19

--- Nico Martin - Dictionary World Map.py
+++ Nico Martin - Dictionary World Map.py
@@ -19,9 +19,19 @@ world_map = {
 # Controller
 playing = True
 current_node = world_map['R19A']
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
+
 while playing:
     print(current_node['NAME'])
     print(current_node['DESCRIPTION'])
     command = input(">_")
     if command.lower() in ['q', 'quit', 'exit']:
         playing = False
+    elif command.upper in directions:
+        try:
+        room_name = current_node['PATHS'][command.upper()]
+        current_node = world_map[room_name]
+        exept KeyError:
+        print("I can't go that way")
+    else:
+        print("Command Not Found")
