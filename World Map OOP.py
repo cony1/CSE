285412bf582f126e18aa0f17354c89b6fc5af8fb
cony1 <42385425+cony1@users.noreply.github.com>
2019-02-22 09:43:14 -0800

    2.22.19

--- World Map OOP.py
+++ World Map OOP.py
@@ -1,5 +1,5 @@
 class Room(object):
-    def __init__(self, name, north=None, south=None, east=None):
+    def __init__(self, name, north=None, south=None, east=None west=None):
         self.name = name
         self.north = north
         self.south = south
