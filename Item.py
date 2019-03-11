d6610ad4bbd65bc329674c81032b1051077a9eed
cony1 <42385425+cony1@users.noreply.github.com>
2019-03-11 09:44:21 -0700

    3.11.19

--- Item.py
+++ Item.py
@@ -2,51 +2,66 @@ class Item(object):
     def __init__(self, name):
         self.name = name
 
+
 class Armor(Item):
-    def __init__(self, name, protection, durability):
+    def __init__(self, name, protection,):
         super(Armor, self).__init__(name)
         self.protection = protection
 
 
 class ChestPlate1(Armor):
     def __init__(self):
-        super(ChestPlate1, self).__init__("Wood Chest Plate, Protection-10", 10)
-
+        super(ChestPlate1, self).__init__("Bronze Chest Plate, Protection-10", 10)
 
 
 class ChestPlate2 (Armor):
     def __init__(self):
-        super(ChestPlate2, self).__init__("Iron Chest Plate, Protection-40", 40)
-
-
-
-class ChestPlate3 (Armor):
-    def __init__(self):
-        super(ChestPlate3, self).__init__("Gold Chest Plate, Protection-60", 60)
-
+        super(ChestPlate2, self).__init__("Iron Chest Plate, Protection - 40", 40)
 
 
 class Leggings1 (Armor):
     def __init__(self):
-        super(Leggings1,self).__init__("Wood Leggings, Protection-10", 10)
-
+        super(Leggings1, self).__init__("Bronze Leggings, Protection - 10", 10)
 
 
 class Leggings2 (Armor):
     def __init__(self):
-        super(Leggings2, self).__init__("Iron Leggings, Protection-40," 40)
-
-
-
-class Leggings3 (Armor):
-    def __init__(self):
-        super(Leggings3, self).__init__("Gold Leggings, Protection-60", 60)
-
+        super(Leggings2, self).__init__("Iron Leggings, Protection - 40", 40)
 
 
 class Boots1 (Armor):
     def __init__(self):
-        super(Leggings1, self).__init__("Wood Boots, Protection-10", 10)
+        super(Boots1, self).__init__("Bronze Boots, Protection - 10", 10)
 
 
-        
+class Boots2 (Armor):
+    def __init__(self):
+        super(Boots2, self).__init__("Iron Boots, Protection - 40", 40)
+
+
+class Weapon(Item):
+    def __init__(self, name, attack):
+        super(Weapon, self).__init__(name)
+        self.attack = attack
+
+
+class Gun1 (Weapon):
+    def __init__(self):
+        super(Gun1, self).__init__("Bronze Handgun, Attack Damage - 10", 10)
+
+
+class Gun2 (Weapon):
+    def __init__(self):
+        super(Gun2, self).__init__("Iron Handgun, Attack Damage - 40", 40)
+
+
+class Crossbow1 (Weapon):
+    def __init__(self):
+        super(Crossbow1, self).__init__("Bronze Crossbow, Attack Damage - 10", 10)
+
+
+class Crossbow2 (Weapon):
+    def __init__(self):
+        super(Crossbow2, self).__init__("Iron Crossbow", 40)
+
+class
